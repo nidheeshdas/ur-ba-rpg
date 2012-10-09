5c24e8c3fbb994df9584ace864c7362ae5d5855a
nidheeshdas@gmail.com <>
2012-10-09 07:16:01 +0000

    gfh

--- src/main/java/com/nirandas/controller/FirstController.java
+++ src/main/java/com/nirandas/controller/FirstController.java
@@ -1,30 +1,50 @@
 package com.nirandas.controller;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 import com.nirandas.beans.Point;
 
 @Controller
 @RequestMapping("/api")
-public class FirstController {
+public class FirstController
+{
 
-	@Autowired
-	public SessionFactory sessionFactory;
+   @Autowired
+   public SessionFactory sessionFactory;
 
-	@RequestMapping(value = "/HelloWorld")
-	public @ResponseBody
-	String HelloWorld() {
-		Session session = sessionFactory.openSession();
-		Transaction trans = session.beginTransaction();
-		session.save(new Point(1, 45, 96));
-		trans.commit();
-		session.close();
-		return sessionFactory.isClosed() ? "Closed" : "Open";
-	}
+   @RequestMapping(value = "/HelloWorld")
+   public @ResponseBody
+   String HelloWorld()
+   {
+      Session session = sessionFactory.openSession();
+      Transaction trans = session.beginTransaction();
+      session.save(new Point(1, 45, 96));
+      trans.commit();
+      session.close();
+      return sessionFactory.isClosed() ? "Closed" : "Open";
+   }
+
+   @RequestMapping("/get")
+   public @ResponseBody String getPoints(Model model)
+   {
+      Session session = sessionFactory.openSession();
+      Transaction trans = session.beginTransaction();
+      Query q = session.createQuery("from Point");
+      List vals = q.list();
+      model.addAttribute("values", vals);
+      trans.commit();
+      session.close();
+      return String.valueOf(vals.size());
+   }
+
 }
